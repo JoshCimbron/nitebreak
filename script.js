c6c06c23d003172c72d21b6dbf3665941bdde2bd
JoshCimbron <44212364+JoshCimbron@users.noreply.github.com>
2021-05-23 09:29:47 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var navbar = document.getElementById('navbar');
+
+if (window.location.pathname.includes ('index.html')){
+    navbar.style.background = "transparent";
+} else {
+    navbar.style.background = "#251B4C";
+}
+
+console.log(window.location.pathname);
