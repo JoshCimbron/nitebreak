66df0e1523d9373a3c5e710daad3ee780f09d890
JoshCimbron <44212364+JoshCimbron@users.noreply.github.com>
2021-05-25 14:28:06 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var navbar = document.getElementById('navbar');
 
-if (window.location.pathname.includes ('index.html') || (window.location.pathname.includes ('index.html'))){
+if (window.location.pathname.includes ('index.html') || (window.location.pathname === ('nitebreak.com'))){
     navbar.style.background = "transparent";
 } else {
     navbar.style.background = "#251B4C";
